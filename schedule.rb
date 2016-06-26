c288514b5e67f9f2ec6f1c00cfed5c7edf4160c8
Adam Jacques <adam@technowizardry.net>
2016-06-26 12:08:52 -0700

    Fix casing on directory change

--- schedule.rb
+++ schedule.rb
@@ -1,4 +1,4 @@
-job_type :backup, 'source /backup/.env && cd /Backup && backup perform -t :task'
+job_type :backup, 'source /backup/.env && cd /backup && backup perform -t :task'
 
 every :sunday, at: '12pm' do
   backup :srv1
